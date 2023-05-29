d300a026002f61f42b84cfba38ee445f93a66df9
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-05-29 20:30:03 +0900

    [Silver IV] Title: 동일한 단어 그룹화하기, Time: 132 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Silver/16499. 동일한 단어 그룹화하기/동일한 단어 그룹화하기.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+import java.util.stream.Collectors;
+
+// 16499 : 동일한 단어 그룹화하기
+
+/**
+ * Example
+ * 4
+ * cat
+ * dog
+ * god
+ * tca
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        // System.setIn(new FileInputStream("/Users/jaeyeonkim/Desktop/CodingTest/CodingTest/BJ/Java/_16499_problem/src/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        List<List<Character>> list = new ArrayList<>();
+        int ans = 0;
+
+        Loop:
+        for (int i = 0; i < N; i++) {
+            char[] c = br.readLine().toCharArray();
+            List<Character> cList = new ArrayList<>();
+            for (char cc : c) {
+                cList.add(cc);
+            }
+            Collections.sort(cList);
+
+            for (List<Character> ll : list) {
+                if (ll.size() != cList.size()) {
+                    continue;
+                }
+
+                boolean equals = true;
+
+                for (int j = 0; j < ll.size(); j++) {
+                    if (!Objects.equals(ll.get(j), cList.get(j))) {
+                        equals = false;
+                        break;
+                    }
+                }
+
+                if (equals) {
+                    continue Loop;
+                }
+            }
+            
+            list.add(cList);
+            ans++;
+        }
+
+        System.out.println(ans);
+    }
+}
