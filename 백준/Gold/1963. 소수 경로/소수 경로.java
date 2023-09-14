10c7731edf2166acbe63b35385926556e911e7a1
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-09-15 01:22:48 +0900

    [Gold IV] Title: 소수 경로, Time: 136 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Gold/1963. 소수 경로/소수 경로.java
+++ 백준/Gold/1963. 소수 경로/소수 경로.java
@@ -1,69 +1,78 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
+// 1963 : 소수 경로
+
+/**
+ * Example
+ * 3
+ * 1033 8179
+ * 1373 8017
+ * 1033 1033
+ */
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // 에라토스테네스의 체
-        // false 가 소수
-        boolean[] prime = new boolean[10000];
-        prime[0] = prime[1] = true;
-        for (int i = 2; i < 10000; i++) {
-            if (!prime[i]) {
-                for (int j = i + i; j < 10000; j += i) {
-                    prime[j] = true;
-                }
+    public static boolean[] isNotPrime = new boolean[10001];
+
+    public static void setPrime() {
+        for (int i = 2; i <= 100; i++) {
+            if (isNotPrime[i]) {
+                continue;
+            }
+
+            for (int j = i * 2; j <= 10000; j += i) {
+                isNotPrime[j] = true;
             }
         }
+    }
 
+    public static void main(String[] args) throws IOException {
+        // System.setIn(new FileInputStream("/Users/jaeyeonkim/Desktop/CodingTest/CodingTest/BJ/Java/_1963_problem/src/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        setPrime();
 
-        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
 
-
-        while (t-- > 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        Loop:
+        while (T-- > 0) {
+            st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            Queue<Integer> q = new LinkedList<>();
-            boolean[] visited = new boolean[10000];// 방문여부 확인
-            int[] count = new int[10000]; // 최소 횟수 저장배열
-
-            // a를 큐에 넣고 방문처리한다
-            q.add(a);
+            Queue<int[]> q = new LinkedList<>();
+            boolean[] visited = new boolean[10001];
             visited[a] = true;
+            q.add(new int[] {a, 0});
 
-            while (!q.isEmpty()) { // 큐가 빌 때까지 반복
-                int num = q.poll(); // 큐에서 값을 꺼낸다
-                for (int i = 0; i < 4; i++) { // 바꿀 자리 반복문 0,1,,2,3번째 자리인 경우
-                    for (int j = 0; j <= 9; j++) { // i번째 자리를 0-9의 값으로 바꾼다
-                        if (i == 0 && j == 0) { // 0번째 자리를 0으로 바꾸면 안된다
+            while (!q.isEmpty()) {
+                int[] p = q.poll();
+
+                if (p[0] == b) {
+                    sb.append(p[1]).append("\n");
+                    continue Loop;
+                }
+
+                for (int i = 1; i <= 1000; i *= 10) {
+                    int standard = p[0] - (((p[0] % (i * 10)) / i) * i);
+
+                    for (int j = 0; j <= 9; j++) {
+                        int now = standard + (j * i);
+
+                        if (now < 1000 || isNotPrime[now] || visited[now]) {
                             continue;
                         }
-                        int k = change(num, i, j); // i번째를 j값으로 변경한 수
-                        if (!prime[k] && !visited[k]) { // k가 소수이고 k를 방문하지 않았을 때
-                            q.add(k); // 큐에 넣고
-                            visited[k] = true; // 방문처리한다
-                            count[k] = count[num] + 1; // 방문횟수를 증가시킨다
-                        }
+
+                        q.add(new int[] {now, p[1] + 1});
+                        visited[now] = true;
                     }
                 }
             }
-            System.out.println(count[b]);
+
+            sb.append(0).append("\n");
         }
 
-
+        System.out.println(sb);
     }
-
-    // 특정 자릿수를 들어온 값으로 바꾸기 위한 함수
-    public static int change(int num, int i, int j) {
-        StringBuilder sb = new StringBuilder(String.valueOf(num)); // int를 stringBuilder로 바꿈
-        sb.setCharAt(i, (char) (j + '0')); //(인덱스, 바꿀문자)  (char) (v + '0') = int v를 문자 v로 만듬
-        return Integer.parseInt(sb.toString()); // 그냥 sb 하면 안되는 이유는 괄호안에 타입이 string이기 때문
-
-    }
-
 }
