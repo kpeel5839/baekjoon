04e7e81b5984d3f7a8ca2b763f3fb789c9cb6f78
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-11-18 23:42:54 +0900

    [level 3] Title: 풍선 터트리기, Time: 26.41 ms, Memory: 133 MB -BaekjoonHub

---
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int solution(int[] a) {
+        // 왼쪽에서 가장 작은애
+        // 오른쪽에서 가장 작은애중
+        // 본인보다 큰 애가 두마리만 아니면 가능한 것
+        int[] leftToRight = new int[a.length];
+        int[] rightToLeft = new int[a.length];
+        leftToRight[0] = a[0];
+        rightToLeft[a.length - 1] = a[a.length - 1];
+        int answer = 0;
+        
+        for (int i = 1; i < a.length; i++) {
+            leftToRight[i] = Math.min(leftToRight[i - 1], a[i]);
+        }
+        
+        for (int i = a.length - 2; 0 <= i; i--) {
+            rightToLeft[i] = Math.min(rightToLeft[i + 1], a[i]);
+        }
+        
+        for (int i = 0; i < a.length; i++) { // 나보다 작은애 둘만 아니면 됨
+            int left = (i == 0 ? Integer.MAX_VALUE : leftToRight[i - 1]);
+            int right = (i == a.length - 1 ? Integer.MAX_VALUE : rightToLeft[i + 1]);
+            
+            if (left < a[i] && right < a[i]) {
+                continue;
+            }
+            
+            answer++;
+        }
+        
+        return answer;
+    }
+}
