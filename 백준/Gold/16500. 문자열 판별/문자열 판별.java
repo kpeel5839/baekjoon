d3be187d5f9232707979a18104a47f043be10b2c
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2023-11-23 20:35:29 +0900

    [Gold V] Title: 문자열 판별, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Gold/16500. 문자열 판별/문자열 판별.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+
+// 16500 : 문자열 판별
+
+/**
+ * Example
+ * softwarecontest
+ * 2
+ * software
+ * contest
+ */
+public class Main {
+
+    public static int N;
+    public static String s;
+    public static String[] a;
+    public static int[] dp;
+
+    public static boolean equals(int index, String compare) {
+        for (int j = 0; j < compare.length(); j++) {
+            if (s.length() <= index + j || s.charAt(index + j) != compare.charAt(j)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static int dfs(int index) {
+        if (s.length() == index) {
+            return 1;
+        }
+
+        if (dp[index] != -1) {
+            return dp[index];
+        }
+
+        dp[index] = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            if (equals(index, a[i]) && dfs(index + a[i].length()) == 1) {
+                dp[index] = 1;
+            }
+        }
+
+        return dp[index];
+    }
+
+    public static void main(String[] args) throws IOException {
+        // System.setIn(new FileInputStream("/Users/jaeyeonkim/Desktop/CodingTest/CodingTest/BJ/Java/_16500_problem/src/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        s = br.readLine();
+        N = Integer.parseInt(br.readLine());
+        a = new String[N];
+        dp = new int[s.length()];
+
+        Arrays.fill(dp, -1);
+
+        for (int i = 0; i < N; i++) {
+            a[i] = br.readLine();
+        }
+
+        System.out.println(dfs(0));
+    }
+}
