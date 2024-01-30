fe6c8dc3305fb442387bcaf75626dad4414c554a
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2024-01-31 00:39:17 +0900

    [Gold II] Title: 물대기, Time: 12 ms, Memory: 5824 KB -BaekjoonHub

--- 백준/Gold/1368. 물대기/물대기.cc
+++ 백준/Gold/1368. 물대기/물대기.cc
@@ -30,92 +30,73 @@ const int inf = 2e9;
 const int SIZE = 1 << 18;
 const int MOD = 1e9 + 7;
 
-int find(int x, vi& parent) {
-  if (parent[x] == x) {
-    return x;
-  }
-  
-  parent[x] = find(parent[x], parent);
-  return parent[x];
-}
-
-void executeUnion(int a, int b, vi& parent) {
-  parent[b] = a;
-}
-
 struct comp {
   bool operator()(vi& o1, vi& o2) {
-    return o1[2] > o2[2];
+    return o1[1] > o2[1];
   }
 };
 
-int bfs(int startNode, vi& non, vii& graph, vi& parent) {
-  priority_queue<vi, vii, comp> q; 
-  int answer = non[startNode];
+int dijkstra(vector<int>& non, vector<vector<int> >& graph, int startNode) {
+  priority_queue<vi, vector<vi>, comp> q;
+  vector<int> visited(non.size(), false);
+  q.push({startNode, non[startNode]});
+  int answer = 0;
+  int visitedCount = 0;
   
-  for (int i = 0; i < graph[startNode].size(); i++) {
-    if (i == startNode) {
+  while (q.size() != 0 && visitedCount != non.size()) {
+    vi vv = q.top();
+    int node = vv[0]; int value = vv[1];
+    q.pop();
+
+    if (visited[node]) {
       continue;
     }
-
-    q.push({startNode, i, min(graph[startNode][i], non[i])});
-  }
-  
-  while (q.size() != 0) {
-    vi p = q.top(); q.pop();
-    int set1 = find(p[0], parent); int set2 = find(p[1], parent);
-
-    if (set1 == set2) {
-      continue;
-    }
-
-    executeUnion(set1, set2, parent);
-    answer += p[2];
+    
+    visitedCount++;
+    visited[node] = true;
+    answer += value;
 
     for (int i = 0; i < non.size(); i++) {
-      if (i == p[1]) {
+      if (i == node) {
+        continue;
+      }
+      
+      if (visited[i]) {
         continue;
       }
 
-      q.push({p[1], i, min(graph[p[1]][i], non[i])});
+      int cost = min(graph[node][i], non[i]);
+      q.push({i, cost});
     }
   }
-  
+
   return answer;
 }
 
 int main(void){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
+//	freopen("input.txt", "r", stdin);
 
   int n; cin >> n;
-  int minNon = inf;
-  int minNonIndex = 0;
-  vi non(n, 0);
-
+  int minNon = inf, minNonIndex = 0;  
+  vector<int> non(n, 0);
   for (int i = 0; i < n; i++) {
     cin >> non[i];
-
+    
     if (non[i] < minNon) {
       minNon = non[i];
-      minNonIndex = i; 
+      minNonIndex = i;
     }
   }
-
-  vii graph(n, vi(n, 0));
   
+  vector<vector<int> > graph(n, vector<int>(n, 0));
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
       cin >> graph[i][j];
     }
   }
-  
-  vi parent(n, 0);
 
-  for (int i = 1; i < n; i++) {
-    parent[i] = i;
-  }
-  
-  cout << bfs(minNonIndex, non, graph, parent);
+  cout << dijkstra(non, graph, minNonIndex);
 	return 0;
 }
