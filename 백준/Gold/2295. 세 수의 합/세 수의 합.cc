cf395d5e360bb066782c1569c16f72d0ca20f791
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2024-02-22 02:00:34 +0900

    [Gold IV] Title: 세 수의 합, Time: 352 ms, Memory: 25520 KB -BaekjoonHub

---
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.cc
@@ -0,0 +1,79 @@
+#include <algorithm>
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <string.h>
+#include <cstdio>
+#include <cmath> 
+#include <set>
+#include <map>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+typedef vector<vii> viii;
+typedef vector<viii> viiii;
+typedef vector<ll> vl;
+typedef vector<vl> vll;
+typedef vector<vll> vlll;
+typedef vector<vlll> vllll;
+typedef vector<char> vc;
+typedef vector<vc> vcc;
+typedef vector<vcc> vccc;
+typedef vector<vccc> vcccc;
+typedef vector<bool> vb;
+typedef vector<vb> vbb;
+typedef vector<vbb> vbbb;
+typedef vector<vbbb> vbbbb;
+const ll INF = 1e18;
+const int inf = 2e9;
+const int size = 1 << 18;
+const int mod = 1e9 + 7;
+
+int N;
+vl arr;
+
+void solve() {
+  cin>>N;
+  arr=vl(N);
+  for(int i=0;i<N;i++){
+    cin>>arr[i];
+  }
+  sort(arr.begin(),arr.end());
+  set<ll>s;
+  for(int i=0;i<N;i++){
+    for(int j=0;j<N;j++){
+      s.insert(arr[i] + arr[j]);
+    }
+  }
+  ll answer=0;
+  for(int i=0;i<N;i++){
+    for(int j=0;j<i;j++){
+      if(s.find(arr[i]-arr[j])!=s.end()){
+        answer=max(answer,arr[i]);
+      }
+    }
+  }
+  cout<<answer;
+}
+
+int main(void){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	// freopen("input.txt", "r", stdin);
+  solve();
+	return 0;
+}
