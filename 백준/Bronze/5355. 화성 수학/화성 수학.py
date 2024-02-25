967f7baa5ca201d575c9afa7fc37f4c71fedd009
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2024-02-25 11:51:58 +0900

    [Bronze II] Title: 화성 수학, Time: 64 ms, Memory: 38256 KB -BaekjoonHub

---
+++ 백준/Bronze/5355. 화성 수학/화성 수학.py
@@ -0,0 +1,28 @@
+import sys
+import math
+import heapq
+from collections import deque, defaultdict
+from itertools import permutations, combinations
+
+INF = 10**18
+inf = 2*10**9
+mod = 10**9 + 7
+
+def solve():
+  n=int(sys.stdin.readline());
+  for i in range(n):
+    v=sys.stdin.readline().strip().split(" ");
+    startValue=float(v[0])
+    for i in range(1,len(v)):
+      if(v[i]=='@'):
+        startValue*=3
+      elif(v[i]=='%'):
+        startValue+=5
+      else:
+        startValue-=7
+    "{:.2f}".format(startValue)
+    sys.stdout.write("{:.2f}".format(startValue));
+    sys.stdout.write("\n");
+
+solve();
+sys.stdout.flush();
