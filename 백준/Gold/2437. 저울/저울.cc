f03d1b083b337c0a6b5c829ca99c2b2f624db4d3
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2024-03-01 16:41:29 +0900

    [Gold II] Title: 저울, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.cc
@@ -0,0 +1,84 @@
+#include <algorithm>
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <string.h>
+#include <cstdio>
+#include <cmath> 
+#include <set>
+#include <map>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+typedef vector<vii> viii;
+typedef vector<viii> viiii;
+typedef vector<ll> vl;
+typedef vector<vl> vll;
+typedef vector<vll> vlll;
+typedef vector<vlll> vllll;
+typedef vector<char> vc;
+typedef vector<vc> vcc;
+typedef vector<vcc> vccc;
+typedef vector<vccc> vcccc;
+typedef vector<bool> vb;
+typedef vector<vb> vbb;
+typedef vector<vbb> vbbb;
+typedef vector<vbbb> vbbbb;
+const ll INF = 1e18;
+const int inf = 2e9;
+const int size = 1 << 18;
+const int mod = 1e9 + 7;
+
+namespace std {
+  template<>
+  struct hash<std::vector<int>> {
+    size_t operator()(const std::vector<int>& v) const {
+      size_t hash_value = 0;
+      for (int i : v) {
+        hash_value ^= std::hash<int>()(i);
+      }
+      return hash_value;
+    }
+  };
+}
+int N;
+void solve() {
+  cin>>N;
+  ll sum=0;
+  vi arr;
+  for(int i=0;i<N;i++){
+    int value;cin>>value;arr.push_back(value);
+  }
+  sort(arr.begin(),arr.end(),[](int o1,int o2){
+    return o1<o2;
+  });
+  for(int i=0;i<N;i++){
+    if(sum+1<arr[i]){
+      cout<<sum+1;
+      return;
+    }
+    sum+=arr[i];
+  }
+  cout<<sum+1<<"\n";
+}
+
+int main(void){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	//freopen("input.txt", "r", stdin);
+  solve();
+}
