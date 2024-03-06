058f82232d78c30a814d3c759bdcc1485a6db0c4
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2024-03-06 15:12:15 +0900

    [Gold III] Title: 양갈래 구하기, Time: 120 ms, Memory: 30888 KB -BaekjoonHub

---
+++ 백준/Gold/31477. 양갈래 구하기/양갈래 구하기.cc
@@ -0,0 +1,94 @@
+#include <algorithm>
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <string.h>
+#include <cstdio>
+#include <cmath> 
+#include <set>
+#include <map>
+
+using namespace std;
+#define y first
+#define x second
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+typedef vector<vii> viii;
+typedef vector<viii> viiii;
+typedef vector<ll> vl;
+typedef vector<vl> vll;
+typedef vector<vll> vlll;
+typedef vector<vlll> vllll;
+typedef vector<char> vc;
+typedef vector<vc> vcc;
+typedef vector<vcc> vccc;
+typedef vector<vccc> vcccc;
+typedef vector<bool> vb;
+typedef vector<vb> vbb;
+typedef vector<vbb> vbbb;
+typedef vector<vbbb> vbbbb;
+const ll INF = 1e18;
+const int inf = 2e9;
+const int size = 1 << 18;
+const int mod = 1e9 + 7;
+
+namespace std {
+  template<>
+  struct hash<std::vector<int>> {
+    size_t operator()(const std::vector<int>& v) const {
+      size_t hash_value = 0;
+      for (int i : v) {
+        hash_value ^= std::hash<int>()(i);
+      }
+      return hash_value;
+    }
+  };
+}
+
+vlll graph;
+int N;
+ll dfs(ll cur,ll parent){
+  ll sum=0;
+  for(vl next:graph[cur]){
+    if(next[0]==parent){
+      continue;
+    }
+    sum+=min(next[1],dfs(next[0],cur));
+  }
+  if(sum==0){
+    return inf;
+  }
+  return sum;
+}
+
+void solve() {
+  cin>>N;
+  graph=vlll(N+1);
+  for(int i=0;i<N-1;i++){
+    int a,b,c;cin>>a>>b>>c;
+    graph[a].push_back({b,c});
+    graph[b].push_back({a,c});
+  }
+  cout<<dfs(1,0);
+}
+
+int main(void){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	//freopen("input.txt", "r", stdin);
+  solve();
+	return 0;
+}
