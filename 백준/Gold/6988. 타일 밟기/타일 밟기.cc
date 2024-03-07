850f0948786e7cafd121f37a73b2d6fc20aeecff
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2024-03-07 14:33:01 +0900

    [Gold I] Title: 타일 밟기, Time: 292 ms, Memory: 72652 KB -BaekjoonHub

---
+++ 백준/Gold/6988. 타일 밟기/타일 밟기.cc
@@ -0,0 +1,94 @@
+#include <algorithm>
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <string.h>
+#include <cstdio>
+#include <cmath> 
+#include <set>
+#include <map>
+
+using namespace std;
+#define y first
+#define x second
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+typedef vector<vii> viii;
+typedef vector<viii> viiii;
+typedef vector<ll> vl;
+typedef vector<vl> vll;
+typedef vector<vll> vlll;
+typedef vector<vlll> vllll;
+typedef vector<char> vc;
+typedef vector<vc> vcc;
+typedef vector<vcc> vccc;
+typedef vector<vccc> vcccc;
+typedef vector<bool> vb;
+typedef vector<vb> vbb;
+typedef vector<vbb> vbbb;
+typedef vector<vbbb> vbbbb;
+const ll INF = 1e18;
+const int inf = 2e9;
+const int size = 1 << 18;
+const int mod = 1e9 + 7;
+
+namespace std {
+  template<>
+  struct hash<std::vector<int>> {
+    size_t operator()(const std::vector<int>& v) const {
+      size_t hash_value = 0;
+      for (int i : v) {
+        hash_value ^= std::hash<int>()(i);
+      }
+      return hash_value;
+    }
+  };
+}
+
+int N;
+vi arr;
+map<int,int>m;
+vll dp;
+void solve() {
+  cin>>N;
+  arr=vi(N);
+  dp=vll(N,vl(N,0));
+  for(int i=0;i<N;i++){
+    cin>>arr[i];
+    m[arr[i]]=i;
+  }
+  ll answer=0;
+  for(int i=1;i<N;i++){
+    for(int j=i-1;0<=j;j--){
+      int diff=arr[i]-arr[j];
+      if(m.find(arr[j]-diff)!=m.end()){
+        dp[i][j]=arr[i]+dp[j][m[arr[j]-diff]];
+        answer=max(answer,dp[i][j]);
+      }else{
+        dp[i][j]=arr[i]+arr[j];
+      }
+    }
+  }
+  cout<<answer;
+}
+
+int main(void){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	//freopen("input.txt", "r", stdin);
+  solve();
+	return 0;
+}
