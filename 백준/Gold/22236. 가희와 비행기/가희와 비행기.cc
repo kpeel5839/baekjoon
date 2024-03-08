fd96cc779baf9936195a5b9a29dae11a26096d08
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2024-03-09 00:05:46 +0900

    [Gold IV] Title: 가희와 비행기, Time: 220 ms, Memory: 127300 KB -BaekjoonHub

---
+++ 백준/Gold/22236. 가희와 비행기/가희와 비행기.cc
@@ -0,0 +1,90 @@
+#include <algorithm>
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <string.h>
+#include <cstdio>
+#include <cmath> 
+#include <set>
+#include <map>
+
+using namespace std;
+#define y first
+#define x second
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+typedef vector<vii> viii;
+typedef vector<viii> viiii;
+typedef vector<ll> vl;
+typedef vector<vl> vll;
+typedef vector<vll> vlll;
+typedef vector<vlll> vllll;
+typedef vector<char> vc;
+typedef vector<vc> vcc;
+typedef vector<vcc> vccc;
+typedef vector<vccc> vcccc;
+typedef vector<bool> vb;
+typedef vector<vb> vbb;
+typedef vector<vbb> vbbb;
+typedef vector<vbbb> vbbbb;
+const ll INF = 1e18;
+const int inf = 2e9;
+const int size = 1 << 18;
+const int mod = 1e9 + 7;
+
+namespace std {
+  template<>
+  struct hash<std::vector<int>> {
+    size_t operator()(const std::vector<int>& v) const {
+      size_t hash_value = 0;
+      for (int i : v) {
+        hash_value ^= std::hash<int>()(i);
+      }
+      return hash_value;
+    }
+  };
+}
+
+ll N,MOD;
+vll dp;
+ll dfs(int idx,int height){
+  if(idx==N){
+    if(height==0){
+      return 1;
+    }
+    return 0;
+  }
+  if(height<=0){
+    return 0;
+  }
+  if(dp[idx][height]!=-1){
+    return dp[idx][height];
+  }
+  return dp[idx][height]=(dfs(idx+1,height+1)+dfs(idx+1,height-1))%MOD;
+}
+void solve() {
+  cin>>N>>MOD;
+  dp=vll(N,vl(N,-1));
+  cout<<dfs(1,1);
+}
+
+int main(void){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	//freopen("input.txt", "r", stdin);
+  solve();
+	return 0;
+}
