c5ac20b7b48d8227c7cb9b054720ac01fc9e755c
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2024-03-15 20:20:26 +0900

    [Silver III] Title: N과 M (7), Time: 492 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,115 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string.h>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+#define y first
+#define x second
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+typedef vector<vii> viii;
+typedef vector<viii> viiii;
+typedef vector<ll> vl;
+typedef vector<vl> vll;
+typedef vector<vll> vlll;
+typedef vector<vlll> vllll;
+typedef vector<char> vc;
+typedef vector<vc> vcc;
+typedef vector<vcc> vccc;
+typedef vector<vccc> vcccc;
+typedef vector<bool> vb;
+typedef vector<vb> vbb;
+typedef vector<vbb> vbbb;
+typedef vector<vbbb> vbbbb;
+const ll INF = 1e18;
+const int inf = 2e9;
+const int size = 1 << 18;
+const int mod = 1e9 + 7;
+
+namespace std {
+  template <> struct hash<std::vector<int>> {
+    size_t operator()(const std::vector<int> &v) const {
+      size_t hash_value = 0;
+      for (int i : v) {
+        hash_value ^= std::hash<int>()(i);
+      }
+      return hash_value;
+    }
+  };
+}
+
+const ll MOD=1e7;
+vl fact = {1};
+ll pow_mod(ll x, ll p) {
+  if (p == 0) {
+    return 1;
+  }
+  if (p % 2 == 0) {
+    ll y = pow_mod(x, p / 2);
+    return (y * y) % MOD;
+  }
+  return (x * pow_mod(x, p - 1)) % MOD;
+}
+ll inv(ll x) {
+  return pow_mod(x, MOD - 2);
+}
+ll cnk(ll n, ll k) {
+  ll res = fact[n];
+  res = (res * inv(fact[k])) % MOD;
+  res = (res * inv(fact[n - k])) % MOD;
+  return res;
+}
+
+int N,M;
+vi arr;
+unordered_set<int>s;
+vi answer;
+void dfs(int idx){
+  if(idx==M){
+    for(auto v:answer){
+      cout<<v<<" ";
+    }
+    cout<<"\n";
+    return;
+  }
+  for(int i=0;i<N;i++){
+    answer.push_back(arr[i]);
+    dfs(idx+1);
+    answer.pop_back();
+  }
+}
+void solve() {
+  cin>>N>>M;
+  for(int i=0;i<N;i++){
+    int a;cin>>a;arr.push_back(a);
+  }
+  sort(arr.begin(),arr.end());
+  dfs(0);
+}
+
+int main(void){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	//freopen("input.txt", "r", stdin);
+  solve();
+	return 0;
+}
