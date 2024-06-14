3d7f234cb43b6d994957bebe55eca077b02c1a48
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2024-06-14 10:41:56 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,12 @@
+a=input()
+if a=='M':
+    print("MatKor")
+elif a=='W':
+    print("WiCys")
+elif a=='C':
+    print("CyKor")
+elif a=='$':
+    print("$clear")
+elif a=='A':
+    print("AlKor")
+    
